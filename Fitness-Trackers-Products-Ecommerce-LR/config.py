227d14612979ce0bb108027117976a09d05567d6
Ajithwinstin <72660875+Ajithwinstin@users.noreply.github.com>
2024-03-21 10:04:31 +0530

    Add files via upload

---
+++ Fitness-Trackers-Products-Ecommerce-LR/config.py
@@ -0,0 +1,7 @@
+DATA_PATH  = r"data/smartwatches.csv"
+EXTRA_COL = 'Unnamed: 0'
+IMP_COL_NUMERICAL = ['Current Price','Original Price','Rating',
+                     'Number OF Ratings','Display Size']
+IMP_COL_CATOGORICAL = ['Brand','Model Name','Dial Shape','Strap Material']
+CLEAN_FILE_PATH  = r"data/clean.csv"
+MODEL_SAVE_NAME  = r"models/model.pkl"
